1a0932bb55aa341303c972cc1a5c2cc0c7b549f6
Mohamed Adel <33991085+Theonlydola@users.noreply.github.com>
2023-01-19 05:02:17 +0200

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,10 @@
 import ProjectCard from "./ProjectCard";
 export default function Projects({ repoList }) {
     return (
-        <div id='projects' className="flex-col overflow-auto justify-center container lg:flex-row">
-            <div>
+          <div>
                 <h1 className="text-4xl font-bold p-5 pb-6 sm:text-6xl">Projects</h1>
-            </div>
+          </div>
+        <div id='projects' className="flex-col overflow-auto justify-center container lg:flex-row">
             <div className="grid gap-4 xl:grid-cols-4 xl:p-20 md:grid-cols-2 md:p-10 sm:grid-cols-1 sm:p-0">
                 {repoList.map((repo, index) => (
                     index < 12 &&
